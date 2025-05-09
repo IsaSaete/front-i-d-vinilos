8a377089081ad6466543312112d1437858899ad0
Isabel Sáenz <isasaete@gmail.com>
2025-05-09 16:11:37 +0200

    Add AppRouter component

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -5,7 +5,6 @@ const NotFoundPAge: React.FC = () => {
     <div className="main-content">
       <h1 className="page-title">Página no encontrada</h1>
       <img
-        className="loader"
         src="/page-not-found.svg"
         alt="Error 404. El 0 está representado con un vinilo"
         width={310}
