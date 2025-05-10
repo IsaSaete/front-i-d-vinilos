53bc1fa33e81875635f02fa298419330b0ca31f2
Isabel Sáenz <isasaete@gmail.com>
2025-05-10 21:58:44 +0200

    Add useVinyls hooks

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
+import { Provider } from "react-redux";
 import AppRouter from "./router/AppRouter";
+import store from "./app/store";
 import "@fontsource-variable/inter/index.css";
 import "./styles/styles.css";
-import { Provider } from "react-redux";
-import store from "./app/store";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
