485afaa6c0b0e3deeec496b311e9a9898adb3cd2
Isabel Sáenz <isasaete@gmail.com>
2025-05-10 22:10:01 +0200

    Add test of loadVinylsByPage function

---
+++ src/vinyl/hooks/__tests__/loadVinylsByPage.test.tsx
@@ -0,0 +1,32 @@
+import { act, renderHook } from "@testing-library/react";
+import { Provider } from "react-redux";
+import useVinyls from "../useVinyls";
+import store from "../../../app/store";
+import { vinylsFixturesDto } from "../../dto/fixturesDto";
+
+describe("Given the loadVinylsByPage function", () => {
+  describe("When it's called with page number 2", () => {
+    test("Then it should show We still Believe vinyl", async () => {
+      const expectedTitleVinyl = "We Still Believe";
+
+      const wrapper = ({ children }: { children: React.ReactNode }) => (
+        <Provider store={store}>{children}</Provider>
+      );
+
+      const { result } = renderHook(() => useVinyls(), { wrapper: wrapper });
+
+      await act(async () => {
+        result.current.loadVinylsByPage(3);
+      });
+
+      const vinylsTotal = result.current.vinylCollection.vinylsTotal;
+      const vinyls = result.current.vinylCollection.vinyls;
+
+      expect(vinylsTotal).toBe(vinylsFixturesDto.length);
+
+      expect(vinyls).toContainEqual(
+        expect.objectContaining({ title: expectedTitleVinyl }),
+      );
+    });
+  });
+});
