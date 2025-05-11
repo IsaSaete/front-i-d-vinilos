7519a84515f809e8b5b119349273cacca9895bde
Isabel Sáenz <isasaete@gmail.com>
2025-05-11 12:24:00 +0200

    Add counter of total vinyls

--- src/vinyl/pages/VinylsPage/VinylsPage.test.tsx
+++ src/vinyl/pages/VinylsPage/VinylsPage.test.tsx
@@ -1,12 +1,20 @@
 import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { MemoryRouter } from "react-router";
 import VinylsPage from "./VinylsPage";
+import store from "../../../app/store";
 
 describe("Given de VinylsPage component", () => {
   describe("When it renders", () => {
     test("Then it should show Vinilos inside a level 1 heading", () => {
       const expectedTitle = /vinilos/i;
 
-      render(<VinylsPage />);
+      render(
+        <Provider store={store}>
+          <VinylsPage />
+        </Provider>,
+        { wrapper: MemoryRouter },
+      );
 
       const pageTitle = screen.getByRole("heading", {
         name: expectedTitle,
--- src/vinyl/pages/VinylsPage/VinylsPage.tsx
+++ src/vinyl/pages/VinylsPage/VinylsPage.tsx
@@ -1,9 +1,29 @@
+import { useSearchParams } from "react-router";
+import { useEffect } from "react";
+import useVinyls from "../../hooks/useVinyls";
 import "./VinylsPage.css";
 
 const VinylsPage: React.FC = () => {
+  const { vinylCollection, loadVinylsByPage } = useVinyls();
+  const [searchParams] = useSearchParams();
+
+  const pageNumber = searchParams.get("page")
+    ? Number(searchParams.get("page"))
+    : 1;
+
+  useEffect(() => {
+    loadVinylsByPage(pageNumber);
+  }, [pageNumber, loadVinylsByPage]);
+
+  const vinylsTotal = vinylCollection.vinylsTotal;
+  const VinylsByPage = vinylCollection.vinyls.length;
+
   return (
     <header className="header-content">
       <h1 className="page-title">Vinilos</h1>
+      <span className="page-counter">
+        {VinylsByPage}/{vinylsTotal}
+      </span>
     </header>
   );
 };
