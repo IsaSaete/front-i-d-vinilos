f3be357cc3d1cfaa1306439c34ca657e404ac4ad
Isabel Sáenz <isasaete@gmail.com>
2025-05-11 10:21:19 +0200

    Add VinylsPage

---
+++ src/vinyl/pages/VinylsPage/VinylsPage.tsx
@@ -0,0 +1,11 @@
+import "./VinylsPage.css";
+
+const VinylsPage: React.FC = () => {
+  return (
+    <header className="header-content">
+      <h1 className="page-title">Vinilos</h1>
+    </header>
+  );
+};
+
+export default VinylsPage;
