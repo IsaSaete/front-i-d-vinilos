ef12e58701388a7233518b3b8651506f4a1f1b2a
Isabel Sáenz <isasaete@gmail.com>
2025-05-18 10:21:59 +0200

    Add test of AddVinylPage

---
+++ src/vinyl/pages/AddVinylPage/AddVinylPage.test.tsx
@@ -0,0 +1,24 @@
+import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import store from "../../../app/store";
+import AddVinylPage from "./AddVinylPage";
+
+describe("Given the AddVinylPage component", () => {
+  describe("When it renders", () => {
+    test("Then it should show 'Añade un vinilo' inside a heading", async () => {
+      const expectedTitle = /añade un vinilo/i;
+      render(
+        <Provider store={store}>
+          <AddVinylPage />
+        </Provider>,
+      );
+
+      const pageTitle = await screen.findByRole("heading", {
+        name: expectedTitle,
+        level: 1,
+      });
+
+      expect(pageTitle).toBeInTheDocument();
+    });
+  });
+});
