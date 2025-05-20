17ddde926b871dedb883bc7cdded7181cf9dde83
Isabel Sáenz <isasaete@gmail.com>
2025-05-20 12:17:45 +0200

    Add test of Modal component

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,14 +1,24 @@
 import { Outlet } from "react-router";
 import { LazyHeader } from "../../router/components/LazyLoader";
+import Modal from "../Modal/Modal";
 import "./Layout.css";
+import useModal from "../../hooks/useModal";
 
 const Layout: React.FC = () => {
+  const {
+    modal: { isSuccess, text, isOpen },
+    closeModal,
+  } = useModal();
+
   return (
     <div className="main-container">
       <LazyHeader />
       <main className="page-container">
         <Outlet />
       </main>
+      {isOpen && (
+        <Modal isSuccess={isSuccess} action={closeModal} text={text} />
+      )}
     </div>
   );
 };
