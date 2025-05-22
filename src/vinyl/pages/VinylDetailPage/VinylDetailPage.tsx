09dcf68b9221f4935a5153a36272db715f71dbc5
Isabel Sáenz <isasaete@gmail.com>
2025-05-22 11:55:37 +0200

    Add ViniylDetailPage component

---
+++ src/vinyl/pages/VinylDetailPage/VinylDetailPage.tsx
@@ -0,0 +1,42 @@
+import { Navigate, useParams } from "react-router";
+import { useEffect } from "react";
+import useVinyls from "../../hooks/useVinyls";
+import { useAppSelector } from "../../../store/hooks";
+import useLoading from "../../../hooks/useLoading";
+import Loader from "../../../components/Loader/Loader";
+import VinylDetail from "../../components/VinylDetail/VinylDetail";
+import "./VinylDetailPage.css";
+
+const VinylDetailPage: React.FC = () => {
+  const { getVinylById } = useVinyls();
+  const {
+    loadingState: { isLoading },
+  } = useLoading();
+  const { id: vinylId } = useParams<{ id: string }>();
+
+  useEffect(() => {
+    window.scroll({ top: 0, behavior: "auto" });
+    getVinylById(vinylId!);
+  }, [vinylId, getVinylById]);
+
+  const vinyl = useAppSelector((state) =>
+    state.vinyls.vinylCollection.vinyls.find((vinyl) => vinyl.id === vinylId),
+  );
+
+  if (isLoading) {
+    return <Loader />;
+  }
+
+  if (!vinyl) {
+    return <Navigate to={"/"} />;
+  }
+
+  return (
+    <>
+      <h1 className="title-page">Info</h1>
+      <VinylDetail vinyl={vinyl} />
+    </>
+  );
+};
+
+export default VinylDetailPage;
