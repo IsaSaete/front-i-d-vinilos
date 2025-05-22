f1d4e242f335ba1c0696d96e612326e4a9786b73
Isabel Sáenz <isasaete@gmail.com>
2025-05-22 19:51:44 +0200

    Add page navigation test

--- src/router/AppRouter.test.tsx
+++ src/router/AppRouter.test.tsx
@@ -1,12 +1,13 @@
 import { render, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router";
 import { Provider } from "react-redux";
+import userEvent from "@testing-library/user-event";
 import AppRouter from "./AppRouter";
 import store from "../store/store";
 
 describe("Given the AppRouter component", () => {
   describe("when it renders in path /vinils that it doesn't exists", () => {
-    test("Then it should show a 'Página no encontrada' inside a heading", async () => {
+    test("Then it should show a 'Página no encontrada' inside a level 1 heading", async () => {
       render(
         <Provider store={store}>
           <MemoryRouter initialEntries={["/vinils"]}>
@@ -21,6 +22,51 @@ describe("Given the AppRouter component", () => {
       });
 
       expect(titlePage).toBeInTheDocument();
+    });
+  });
+
+  describe("When it renders in path /vinilos", () => {
+    test("Then it should show 'Vinilos' inside a level 1 heading", async () => {
+      render(
+        <Provider store={store}>
+          <MemoryRouter initialEntries={["/vinilos"]}>
+            <AppRouter />
+          </MemoryRouter>
+        </Provider>,
+      );
+
+      const titleVinylsPage = await screen.findByRole("heading", {
+        name: /vinilos/i,
+        level: 1,
+      });
+
+      expect(titleVinylsPage).toBeInTheDocument();
+    });
+
+    describe("And the user clicks on 'Añadir' link", () => {
+      test("Then it should show 'Añade un vinilo' inside a level 1 heading", async () => {
+        render(
+          <Provider store={store}>
+            <MemoryRouter initialEntries={["/vinilos"]}>
+              <AppRouter />
+            </MemoryRouter>
+          </Provider>,
+        );
+
+        const addVinylLink = await screen.findByRole("link", {
+          name: /añadir/i,
+        });
+
+        expect(addVinylLink).toBeInTheDocument();
+
+        await userEvent.click(addVinylLink);
+
+        const titleAddPage = await screen.findByRole("heading", {
+          name: /añade un vinilo/i,
+        });
+
+        expect(titleAddPage).toBeInTheDocument();
+      });
     });
   });
 });
