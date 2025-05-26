5146dc524ee547baec22e4ec70d078b5ce846840
Isabel Sáenz <isasaete@gmail.com>
2025-05-26 12:27:37 +0200

    Add Footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import "./Footer.css";
+
+const Footer: React.FC = () => {
+  return (
+    <div className="footer-content">
+      <span>© 2025 I+D Vinilos. Todos los derechos reservados.</span>
+    </div>
+  );
+};
+
+export default Footer;
